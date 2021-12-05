87b7d582bc8887259261db6208a8e73ec94a6cf3
PhilMinecraft <67897800+PhilMinecraft@users.noreply.github.com>
2021-12-05 18:46:53 +0800

    Add files via upload

---
+++ js/assets/sidebar.js
@@ -0,0 +1,8 @@
+function openSlideMenu(){
+    document.getElementById('mobile-sidebar-area').style.marginLeft = '255px';
+    document.getElementById('mobile-sidebar_button-open').style.marginLeft = '0';
+  }
+  function closeSlideMenu(){
+    document.getElementById('mobile-sidebar-area').style.marginLeft = '0';
+    document.getElementById('mobile-sidebar_button-open').style.marginLeft = '0';
+  }
