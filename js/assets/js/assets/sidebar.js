23b99124fcdff5f52e94a7b47df022f4ab8c484d
PhilMinecraft <67897800+PhilMinecraft@users.noreply.github.com>
2021-12-10 12:25:40 +0800

    Delete js/assets/js/assets directory

--- js/assets/js/assets/sidebar.js
+++
@@ -1,6 +0,0 @@
-function openSidebar(){
-    document.getElementById('mobile-sidebar-area').style.marginLeft = '255px';
-  }
-  function closeSidebar(){
-    document.getElementById('mobile-sidebar-area').style.marginLeft = '0';
-  }
