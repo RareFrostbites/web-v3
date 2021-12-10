83c753baac0e3731984057615438237fc203faec
PhilMinecraft <67897800+PhilMinecraft@users.noreply.github.com>
2021-12-10 12:25:24 +0800

    Add files via upload

---
+++ js/assets/js/assets/header.js
@@ -0,0 +1,6 @@
+function openProducts(){
+    document.getElementById('open-products').style.display = 'block';
+  }
+  function closeProducts(){
+    document.getElementById('open-products').style.display = 'none';
+  }
---
+++ js/assets/js/assets/sidebar.js
@@ -0,0 +1,6 @@
+function openSidebar(){
+    document.getElementById('mobile-sidebar-area').style.marginLeft = '255px';
+  }
+  function closeSidebar(){
+    document.getElementById('mobile-sidebar-area').style.marginLeft = '0';
+  }
