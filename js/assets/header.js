7caf18c0b221929be9a0af84d12152ca64c76e00
PhilMinecraft <67897800+PhilMinecraft@users.noreply.github.com>
2021-12-16 15:34:49 +0800

    Add files via upload

--- js/assets/header.js
+++ js/assets/header.js
@@ -1,6 +1,9 @@
-function openProducts(){
-    document.getElementById('open-products').style.display = 'block';
+window.onscroll = function() {headerScroll()};
+
+function headerScroll() {
+  if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
+    document.getElementById("header").className = "header_shadow";
+  } else {
+    document.getElementById("header").className = "header";
   }
-  function closeProducts(){
-    document.getElementById('open-products').style.display = 'none';
-  }
+}
