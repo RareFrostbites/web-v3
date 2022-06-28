0c398a3c99481e198982718dfe4a6facd1f790bf
PhilMinecraft <arturo.rabino.3@outlook.com>
2022-06-28 15:24:07 +0800

    Add files via upload

--- js/store/language/pi-m.js
+++ js/store/language/pi-m.js
@@ -3,10 +3,12 @@ const packinfo = {
         // Tags
         "tags" : "TAGS:",
 
+        "tags_i-Fast" : "Fast",
         "tags_i-Gang" : "Gang",
         "tags_i-Group" : "Group",
         "tags_i-Pixel_Art" : "Pixel Art",
         "tags_i-Prank" : "Prank",
+        "tags_i-Race" : "Race",
         "tags_i-Stylized" : "Stylized",
         "tags_i-Troll" : "Troll",
 
@@ -18,6 +20,7 @@ const packinfo = {
         "genres_i-Christmas" : "Christmas",
         "genres_i-Funny" : "Funny",
         "genres_i-Horror" : "Horror",
+        "genres_i-Sports" : "Sports",
 
         // Players
         "players" : "PLAYERS:",
@@ -28,10 +31,12 @@ const packinfo = {
         // Tags
         "tags" : "TAGS:",
 
+        "tags_i-Fast" : "Fast",
         "tags_i-Gang" : "Gang",
         "tags_i-Group" : "Group",
         "tags_i-Pixel_Art" : "Pixel Art",
         "tags_i-Prank" : "Prank",
+        "tags_i-Race" : "Race",
         "tags_i-Stylized" : "Stylized",
         "tags_i-Troll" : "Troll",
 
@@ -43,6 +48,7 @@ const packinfo = {
         "genres_i-Christmas" : "Christmas",
         "genres_i-Funny" : "Funny",
         "genres_i-Horror" : "Horror",
+        "genres_i-Sports" : "Sports",
 
         // Players
         "players" : "PLAYERS:",
@@ -53,10 +59,12 @@ const packinfo = {
         // Tags
         "tags" : "ETIQUETAS:",
 
+        "tags_i-Fast" : "Rápido",
         "tags_i-Gang" : "Pandilla",
         "tags_i-Group" : "Grupo",
         "tags_i-Pixel_Art" : "Arte de Píxeles",
         "tags_i-Prank" : "Broma",
+        "tags_i-Race" : "Raza",
         "tags_i-Stylized" : "Estilizado",
         "tags_i-Troll" : "Troll",
 
@@ -68,6 +76,7 @@ const packinfo = {
         "genres_i-Christmas" : "Navidad",
         "genres_i-Funny" : "Gracioso",
         "genres_i-Horror" : "Horror",
+        "genres_i-Sports" : "Deportivo",
 
         // Players
         "players" : "JUGADORES:",
@@ -78,10 +87,12 @@ const packinfo = {
         // Tags
         "tags" : "MGA TAG:",
 
+        "tags_i-Fast" : "Mabilis",
         "tags_i-Gang" : "Gang",
         "tags_i-Group" : "Grupo",
         "tags_i-Pixel_Art" : "Pixel Art",
         "tags_i-Prank" : "Prank",
+        "tags_i-Race" : "Race",
         "tags_i-Stylized" : "Stylized",
         "tags_i-Troll" : "Troll",
 
@@ -93,6 +104,7 @@ const packinfo = {
         "genres_i-Christmas" : "Christmas",
         "genres_i-Funny" : "Nakaka-funny",
         "genres_i-Horror" : "Horror",
+        "genres_i-Sports" : "Mga Sport",
 
         // Players
         "players" : "MGA PLAYER:",
@@ -103,10 +115,12 @@ const packinfo = {
         // Tags
         "tags" : "टैग:",
 
+        "tags_i-Fast" : "तेज",
         "tags_i-Gang" : "दल",
         "tags_i-Group" : "समूहीकृत",
         "tags_i-Pixel_Art" : "पिक्सेल कला",
         "tags_i-Prank" : "शरारत",
+        "tags_i-Race" : "जाति",
         "tags_i-Stylized" : "शैलीबद्ध",
         "tags_i-Troll" : "ट्रोल",
 
@@ -118,6 +132,7 @@ const packinfo = {
         "genres_i-Christmas" : "क्रिसमस",
         "genres_i-Funny" : "मजे़दार",
         "genres_i-Horror" : "दहशत",
+        "genres_i-Sports" : "खेल-कूद",
 
         // Players
         "players" : "खिलाड़ियों:",
@@ -128,10 +143,12 @@ const packinfo = {
         // Tags
         "tags" : "TAGS:",
 
+        "tags_i-Fast" : "Cepat",
         "tags_i-Gang" : "Geng",
         "tags_i-Group" : "Kelompok",
         "tags_i-Pixel_Art" : "Seni Piksel",
         "tags_i-Prank" : "Lelucon",
+        "tags_i-Race" : "Lomba",
         "tags_i-Stylized" : "Bergaya",
         "tags_i-Troll" : "Troll",
 
@@ -143,6 +160,7 @@ const packinfo = {
         "genres_i-Christmas" : "Natal",
         "genres_i-Funny" : "Lucu",
         "genres_i-Horror" : "Horor",
+        "genres_i-Sports" : "Olahraga",
 
         // Players
         "players" : "PEMAIN:",
@@ -153,10 +171,12 @@ const packinfo = {
         // Tags
         "tags" : "タグ：",
 
+        "tags_i-Fast" : "速い",
         "tags_i-Gang" : "一団",
         "tags_i-Group" : "集団",
         "tags_i-Pixel_Art" : "ピクセルアート",
         "tags_i-Prank" : "悪ふざけ",
+        "tags_i-Race" : "競走",
         "tags_i-Stylized" : "定型 化 された",
         "tags_i-Troll" : "トロール",
 
@@ -168,6 +188,7 @@ const packinfo = {
         "genres_i-Christmas" : "クリスマス",
         "genres_i-Funny" : "可笑しい",
         "genres_i-Horror" : "ホラー",
+        "genres_i-Sports" : "スポーツ",
 
         // Players
         "players" : "選手：",
@@ -178,10 +199,12 @@ const packinfo = {
         // Tags
         "tags" : "ស្លាក:",
 
+        "tags_i-Fast" : "លឿន",
         "tags_i-Gang" : "ក្រុម",
         "tags_i-Group" : "ក្រុម",
         "tags_i-Pixel_Art" : "គំនូរ Pixel",
         "tags_i-Prank" : "Prank",
+        "tags_i-Race" : "ប្រណាំង",
         "tags_i-Stylized" : "រចនាប័ទ្ម",
         "tags_i-Troll" : "Troll",
 
@@ -193,6 +216,7 @@ const packinfo = {
         "genres_i-Christmas" : "បុណ្យណូអែល",
         "genres_i-Funny" : "សើចចុកពោះ",
         "genres_i-Horror" : "រន្ធត់ណាស់",
+        "genres_i-Sports" : "កីឡា",
 
         // Players
         "players" : "អ្នកលេង៖",
@@ -203,10 +227,12 @@ const packinfo = {
         // Tags
         "tags" : "TAGS:",
 
+        "tags_i-Fast" : "빠른",
         "tags_i-Gang" : "갱",
         "tags_i-Group" : "그룹",
         "tags_i-Pixel_Art" : "픽셀 아트",
         "tags_i-Prank" : "장난",
+        "tags_i-Race" : "인종",
         "tags_i-Stylized" : "양식에 일치 시키는",
         "tags_i-Troll" : "트롤",
 
@@ -218,6 +244,7 @@ const packinfo = {
         "genres_i-Christmas" : "크리스마스",
         "genres_i-Funny" : "우습다",
         "genres_i-Horror" : "공포",
+        "genres_i-Sports" : "스포츠",
 
         // Players
         "players" : "PLAYERS:",
@@ -228,10 +255,12 @@ const packinfo = {
         // Tags
         "tags" : "TAGS:",
 
+        "tags_i-Fast" : "Cepat",
         "tags_i-Gang" : "Geng",
         "tags_i-Group" : "Kumpulan",
         "tags_i-Pixel_Art" : "Lukisan Piksel",
         "tags_i-Prank" : "Prank",
+        "tags_i-Race" : "Perlumbaan",
         "tags_i-Stylized" : "Digayakan",
         "tags_i-Troll" : "Troll",
 
@@ -243,6 +272,7 @@ const packinfo = {
         "genres_i-Christmas" : "Krismas",
         "genres_i-Funny" : "Lucu",
         "genres_i-Horror" : "Seram",
+        "genres_i-Sports" : "Sukan",
 
         // Players
         "players" : "PEMAIN:",
@@ -253,10 +283,12 @@ const packinfo = {
         // Tags
         "tags" : "တက်ဂ်များ",
 
+        "tags_i-Fast" : "မြန်မြန်ဆန်ဆန်Rápido",
         "tags_i-Gang" : "ဂိုဏ်းဂဏန်း",
         "tags_i-Group" : "အုပ်စု",
         "tags_i-Pixel_Art" : "Pixel Art",
         "tags_i-Prank" : "ပရန့်ခ်",
+        "tags_i-Race" : "ပြိုင်ပွဲ",
         "tags_i-Stylized" : "စတိုင်လစ်",
         "tags_i-Troll" : "Trolll",
 
@@ -268,6 +300,7 @@ const packinfo = {
         "genres_i-Christmas" : "ခရစ္စမတ်",
         "genres_i-Funny" : "ရယ်စရာ",
         "genres_i-Horror" : "ထိတ်လန့်မှု",
+        "genres_i-Sports" : "အားကစား",
 
         // Players
         "players" : "ကစားသမားများ-",
@@ -278,10 +311,12 @@ const packinfo = {
         // Tags
         "tags" : "ETIQUETAS:",
 
+        "tags_i-Fast" : "Rápido",
         "tags_i-Gang" : "Gangue",
         "tags_i-Group" : "Grupo",
         "tags_i-Pixel_Art" : "Pixel Art",
         "tags_i-Prank" : "Brincadeira",
+        "tags_i-Race" : "Raça",
         "tags_i-Stylized" : "Estilizado",
         "tags_i-Troll" : "Troll",
 
@@ -293,6 +328,7 @@ const packinfo = {
         "genres_i-Christmas" : "Natal",
         "genres_i-Funny" : "Engraçado",
         "genres_i-Horror" : "Horror",
+        "genres_i-Sports" : "Esportes",
 
         // Players
         "players" : "JOGADORES:",
@@ -303,10 +339,12 @@ const packinfo = {
         // Tags
         "tags" : "ТЕГИ:",
 
+        "tags_i-Fast" : "Быстрый",
         "tags_i-Gang" : "Банда",
         "tags_i-Group" : "Группа",
         "tags_i-Pixel_Art" : "Пиксельная Графика",
         "tags_i-Prank" : "Проделка",
+        "tags_i-Race" : "Раса",
         "tags_i-Stylized" : "Стилизованный",
         "tags_i-Troll" : "Тролль",
 
@@ -318,6 +356,7 @@ const packinfo = {
         "genres_i-Christmas" : "Рождество",
         "genres_i-Funny" : "Забавный",
         "genres_i-Horror" : "Ужас",
+        "genres_i-Sports" : "Спортивный",
 
         // Players
         "players" : "ИГРОКОВ:",
@@ -328,10 +367,12 @@ const packinfo = {
         // Tags
         "tags" : "TAGS:",
 
+        "tags_i-Fast" : "Nhanh",
         "tags_i-Gang" : "Bọn",
         "tags_i-Group" : "Nhóm",
         "tags_i-Pixel_Art" : "Pixel Art",
         "tags_i-Prank" : "Prank",
+        "tags_i-Race" : "Chủng tộc",
         "tags_i-Stylized" : "Cách điệu",
         "tags_i-Troll" : "Quỷ khổng lồ",
 
@@ -343,6 +384,7 @@ const packinfo = {
         "genres_i-Christmas" : "Giáng sinh",
         "genres_i-Funny" : "Buồn cười",
         "genres_i-Horror" : "Sự ghê rợn",
+        "genres_i-Sports" : "Thể thao",
 
         // Players
         "players" : "CẦU THỦ:",
@@ -353,10 +395,12 @@ const packinfo = {
         // Tags
         "tags" : "标签：",
 
+        "tags_i-Fast" : "快",
         "tags_i-Gang" : "岗",
         "tags_i-Group" : "小组",
         "tags_i-Pixel_Art" : "像素艺术",
         "tags_i-Prank" : "恶作剧",
+        "tags_i-Race" : "比赛",
         "tags_i-Stylized" : "风格",
         "tags_i-Troll" : "钓鱼",
 
@@ -368,6 +412,7 @@ const packinfo = {
         "genres_i-Christmas" : "圣诞节",
         "genres_i-Funny" : "有趣",
         "genres_i-Horror" : "恐怖",
+        "genres_i-Sports" : "体育",
 
         // Players
         "players" : "球员：",
@@ -378,10 +423,12 @@ const packinfo = {
         // Tags
         "tags" : "标签：",
 
+        "tags_i-Fast" : "快",
         "tags_i-Gang" : "岗",
         "tags_i-Group" : "小组",
         "tags_i-Pixel_Art" : "像素艺术",
         "tags_i-Prank" : "恶作剧",
+        "tags_i-Race" : "比赛",
         "tags_i-Stylized" : "风格",
         "tags_i-Troll" : "钓鱼",
 
@@ -393,6 +440,7 @@ const packinfo = {
         "genres_i-Christmas" : "圣诞节",
         "genres_i-Funny" : "有趣",
         "genres_i-Horror" : "恐怖",
+        "genres_i-Sports" : "体育",
 
         // Players
         "players" : "球员：",
