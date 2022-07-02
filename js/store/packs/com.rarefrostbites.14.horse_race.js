351ec16d9bbf62a2698d6ae4187118ba18678b10
PhilMinecraft <arturo.rabino.3@outlook.com>
2022-07-02 13:28:31 +0800

    Add files via upload

--- js/store/packs/com.rarefrostbites.14.horse_race.js
+++ js/store/packs/com.rarefrostbites.14.horse_race.js
@@ -1,21 +1,3 @@
-function PackLanguage() {
-  // Packs
-  var initLang = languagePack[LanguageChecker()];
-  for (var lang of Object.keys(initLang)) {
-    if ($("[data-" + lang + "]")) {
-      $("[data-" + lang + "]").html(initLang[lang]);
-    }
-  }
-  
-  // Pack info assets
-  var initLang = packinfo[LanguageChecker()];
-  for (var lang of Object.keys(initLang)) {
-    if ($("[data-" + lang + "]")) {
-      $("[data-" + lang + "]").html(initLang[lang]);
-    }
-  }
-}
-
 "use strict";
 
 const languagePack = {
@@ -99,58 +81,4 @@ const languagePack = {
     "pa": "RareFrostbites Team",
     "pd": "Experience an extreme sport with your friends, race in an icy and dangerous race track with hurdles. Show your sportsmanship!"
   }
-}
-
-let timeoutcopylink;
-let copylink = (copy) => {
-  $("#alert-dl").hide();
-  clearTimeout(timeoutcopylink);
-  document.getElementById(copy).select();
-  document.execCommand("copy");
-  document.getElementById("alert-cl").style.display = "flex";
-  timeoutcopylink = setTimeout(hidecopylink, 3000);
-};
-function hidecopylink() {
-  $("#alert-cl").hide();
-};
-
-let timeoutdownloadpack;
-let downloadpack = () => {
-  $("#b").hide();
-  document.getElementById("b-blocked").style.display = "flex";
-  $("#alert-cl").hide();
-  clearTimeout(timeoutdownloadpack);
-  document.getElementById("alert-dl").style.display = "flex";
-  timeoutdownloadpack = setTimeout(hidedownloadpack, 3000);
-};
-function hidedownloadpack() {
-  $("#alert-dl").hide();
-};
-
-
-function readMorePD() {
-  document.getElementById("button-d").onclick = readLessPD;
-  document.getElementById("pi-d").style.whiteSpace = "normal";
-  document.getElementById("show-d").innerHTML = '-';
-}
-function readLessPD() {
-  document.getElementById("button-d").onclick = readMorePD;
-  document.getElementById("pi-d").style.whiteSpace = "nowrap";
-  document.getElementById("show-d").innerHTML = '+';
-}
-
-
-function readMoreCHLGS() {
-  document.getElementById("button-chlgs").onclick = readLessCHLGS;
-  document.getElementById("pi-chlgs-unread").style.whiteSpace = "normal";
-  document.getElementById("pi-chlgs-unread").id = "pi-chlgs-read";
-  $("#pi-cr").hide()
-  document.getElementById("show-chlgs").innerHTML = '-';
-}
-function readLessCHLGS() {
-  document.getElementById("button-chlgs").onclick = readMoreCHLGS;
-  document.getElementById("pi-chlgs-read").style.whiteSpace = "nowrap";
-  document.getElementById("pi-chlgs-read").id = "pi-chlgs-unread";
-  document.getElementById("pi-cr").style.display = "flex";
-  document.getElementById("show-chlgs").innerHTML = '+';
 }
