5334d657d55715f9829c53129c1f6001a9cb31db
PhilMinecraft <arturo.rabino.3@outlook.com>
2022-07-04 21:24:36 +0800

    Add files via upload

--- js/store/language/initialize.js
+++ js/store/language/initialize.js
@@ -4,81 +4,97 @@ const languageContent = {
   "en-gb": {
     "pp-f": "FREE",
     "pg": "Download",
-    "cl": "Link copied"
+    "cl": "Link copied",
+    "lp": "Latest Packs"
   },
   "en-us": {
     "pp-f": "FREE",
     "pg": "Download",
-    "cl": "Link copied"
+    "cl": "Link copied",
+    "lp": "Latest Packs"
   },
   "es-es": {
     "pp-f": "GRATIS",
     "pg": "Descargar",
-    "cl": "Enlace copiado"
+    "cl": "Enlace copiado",
+    "lp": "Últimos Packs"
   },
   "fil-ph": {
     "pp-f": "LIBRE",
     "pg": "I-download",
-    "cl": "Na-copy ang link"
+    "cl": "Na-copy ang link",
+    "lp": "Mga Latest na Pack"
   },
   "hi-in": {
     "pp-f": "उचित",
     "pg": "डाउनलोड",
-    "cl": "लिंक क्लिपबोर्ड में लग चुकी है"
+    "cl": "लिंक क्लिपबोर्ड में लग चुकी है",
+    "lp": "नवीनतम पैक"
   },
   "id-id": {
     "pp-f": "GRATIS",
     "pg": "Unduh",
-    "cl": "Tautan disalin"
+    "cl": "Tautan disalin",
+    "lp": "Paket Terbaru"
   },
   "ja-jp": {
     "pp-f": "無料",
     "pg": "ダウンロード",
-    "cl": "コピーされたリンク"
+    "cl": "コピーされたリンク",
+    "lp": "最新パック"
   },
   "km-kh": {
     "pp-f": "ಉಚಿತឥតគិតថ្លៃ",
     "pg": "ទាញយក",
-    "cl": "តំណ ដែល បាន ចម្លង"
+    "cl": "តំណ ដែល បាន ចម្លង",
+    "lp": "កញ្ចប់ ចុងក្រោយ បំផុត"
   },
   "ko-kr": {
     "pp-f": "무료",
     "pg": "다운로드",
-    "cl": "링크 복사됨"
+    "cl": "링크 복사됨",
+    "lp": "최신 팩"
   },
   "ms-my": {
     "pp-f": "PERCUMA",
     "pg": "Muat turun",
-    "cl": "Pautan disalin"
+    "cl": "Pautan disalin",
+    "lp": "Pek Terkini"
   },
   "my-mm": {
     "pp-f": "လွတ်လပ်မှု",
     "pg": "ဒေါင်းလုတ်ဆွဲပါ",
-    "cl": "လင့်ခ်ကူးယူထား"
+    "cl": "လင့်ခ်ကူးယူထား",
+    "lp": "နောက်ဆုံးပက်ခ်များ"
   },
   "pt-pt": {
     "pp-f": "GRÁTIS",
     "pg": "Baixar",
-    "cl": "Ligação copiada"
+    "cl": "Ligação copiada",
+    "lp": "Pacotes mais Recentes"
   },
   "ru-ru": {
     "pp-f": "СВОБОДНЫЙ",
     "pg": "Загружать",
-    "cl": "Ссылка скопирована"
+    "cl": "Ссылка скопирована",
+    "lp": "Последние Пакеты"
   },
   "vi-vn": {
     "pp-f": "TỰ DO",
     "pg": "Tải xuống",
-    "cl": "Nối kết được sao chép"
+    "cl": "Nối kết được sao chép",
+    "lp": "Gói mới Nhất"
   },
   "zh-cn": {
     "pp-f": "自由",
     "pg": "下载",
-    "cl": "链接已复制"
+    "cl": "链接已复制",
+    "lp": "最新包装"
   },
   "zh-tw": {
     "pp-f": "自由",
     "pg": "下载",
-    "cl": "链接已复制"
+    "cl": "链接已复制",
+    "lp": "最新包装"
   }
 };
