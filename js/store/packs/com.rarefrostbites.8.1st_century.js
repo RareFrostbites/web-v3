042f3202050b0ff7ec6c21cbbe9b1e77e49efa03
PhilMinecraft <arturo.rabino.3@outlook.com>
2022-07-06 08:39:17 +0800

    Add files via upload

---
+++ js/store/packs/com.rarefrostbites.8.1st_century.js
@@ -0,0 +1,84 @@
+"use strict";
+
+const languagePack = {
+  "en-gb": {
+    "pt": "1st Century",
+    "pa": "RareFrostbites Team",
+    "pd": "Troll your friends into a dark and scary place.  Get your head cut off, and shout!"
+  },
+  "en-us": {
+    "pt": "1st Century",
+    "pa": "RareFrostbites Team",
+    "pd": "Troll your friends into a dark and scary place.  Get your head cut off, and shout!"
+  },
+  "es-es": {
+    "pt": "1st Century",
+    "pa": "RareFrostbites Team",
+    "pd": "Troll your friends into a dark and scary place.  Get your head cut off, and shout!"
+  },
+  "fil-ph": {
+    "pt": "1st Century",
+    "pa": "RareFrostbites Team",
+    "pd": "I-troll ang iyong mga kaibigan sa isang madilim at nakakatakot na lugar. Ihanda ang iyong ulo'y maputol, at sumigaw!"
+  },
+  "hi-in": {
+    "pt": "1st Century",
+    "pa": "RareFrostbites Team",
+    "pd": "अपने दोस्तों को एक अंधेरी और डरावनी जगह पर ट्रोल करें।  अपना सिर काटो, और चिल्लाओ!"
+  },
+  "id-id": {
+    "pt": "1st Century",
+    "pa": "RareFrostbites Team",
+    "pd": "Troll your friends into a dark and scary place.  Get your head cut off, and shout!"
+  },
+  "ja-jp": {
+    "pt": "1st Century",
+    "pa": "RareFrostbites Team",
+    "pd": "暗くて怖い場所に友達を荒らします。 頭を切り落として叫びなさい！"
+  },
+  "km-kh": {
+    "pt": "1st Century",
+    "pa": "RareFrostbites Team",
+    "pd": "Troll your friends into a dark and scary place.  Get your head cut off, and shout!"
+  },
+  "ko-kr": {
+    "pt": "1st Century",
+    "pa": "RareFrostbites Team",
+    "pd": "친구들을 어둡고 무서운 곳으로 트롤하십시오.  머리를 자르고 소리 치세요!"
+  },
+  "ms-my": {
+    "pt": "1st Century",
+    "pa": "RareFrostbites Team",
+    "pd": "Troll your friends into a dark and scary place.  Get your head cut off, and shout!"
+  },
+  "my-mm": {
+    "pt": "1st Century",
+    "pa": "RareFrostbites Team",
+    "pd": "Troll your friends into a dark and scary place.  Get your head cut off, and shout!"
+  },
+  "pt-pt": {
+    "pt": "1st Century",
+    "pa": "RareFrostbites Team",
+    "pd": "Troll seus amigos em um lugar escuro e assustador.  Corte sua cabeça e grite!"
+  },
+  "ru-ru": {
+    "pt": "1st Century",
+    "pa": "RareFrostbites Team",
+    "pd": "Troll your friends into a dark and scary place.  Get your head cut off, and shout!"
+  },
+  "vi-vn": {
+    "pt": "1st Century",
+    "pa": "RareFrostbites Team",
+    "pd": "Troll your friends into a dark and scary place.  Get your head cut off, and shout!"
+  },
+  "zh-cn": {
+    "pt": "1st Century",
+    "pa": "RareFrostbites Team",
+    "pd": "将您的朋友拖到一个黑暗而恐怖的地方。 砍下头，大喊！"
+  },
+  "zh-tw": {
+    "pt": "1st Century",
+    "pa": "RareFrostbites Team",
+    "pd": "Troll your friends into a dark and scary place.  Get your head cut off, and shout!"
+  }
+}
